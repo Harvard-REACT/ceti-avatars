12604b5dec9a5620f87362cb1f354c3f85288958
Hammad Izhar <hizhar@g.harvard.edu>
2025-06-10 23:07:36 -0400

    feat: attempt to deploy pages site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    site: "https://harvard-react.github.io",
+    base: "/ceti-avatars",
+    integrations: [react()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
 });
